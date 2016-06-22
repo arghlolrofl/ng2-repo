f743408d6269487ed3050e2158f3e82e77292c05
Dominik Weidenfeld <dominik.weidenfeld@twt.de>
2016-06-22 14:43:50 +0200

    api retry logic

---
+++ app/utils/rx-utils.ts
@@ -0,0 +1,20 @@
+import {Observable} from 'rxjs/Observable';
+
+/**
+ * RX Utilities.
+ */
+export default class RxUtils {
+
+    /**
+     * Error delay for reactive tasks.
+     * @param {number} retries number of retries
+     * @param {number} delay number of milliseconds to wait between retries
+     * @returns {function(Observable): Observable<any>}
+     */
+    public static errorDelay(retries, delay) {
+        return (errors:Observable) =>
+            Observable.range(1, retries)
+                .zip(errors, (i) => i)
+                .mergeMap((i) => Observable.timer(delay));
+    }
+}
