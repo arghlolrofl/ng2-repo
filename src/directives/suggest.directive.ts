cd83fefc90eb8ae0e8cdb1343270cfe0c0b965ac
Dominik Weidenfeld <dominik.weidenfeld@twt.de>
2016-07-13 14:58:58 +0200

    added auto hide function for suggestions

--- src/directives/suggest.directive.ts
+++ src/directives/suggest.directive.ts
@@ -13,48 +13,63 @@ export class SuggestDirective implements OnInit {
     /**
      * Maps the API logic.
      */
-    @Input('fpSuggest')
-    public mapper:(term:string) => any;
+    @Input('fpSuggest') mapper: (term: string) => any;
 
     /**
      * The event and message queue.
      */
-    @Input('fpSuggestEvents')
-    public events:EventEmitter<any>;
+    @Input('fpSuggestEvents') events: EventEmitter<any>;
+
+    /**
+     * Element for viewport check.
+     */
+    @Input('fpSuggestView') view: Element;
 
     /**
      * Suggestion list to be stored.
      * @type {Array}
      */
-    private suggestions:any[] = [];
+    suggestions: any[] = [];
 
     /**
      * Offset of selected / active suggestion.
      * @type {number}
      */
-    private offset:number = 0;
+    offset: number = 0;
 
     /**
      * True if some selection is active.
      * @type {boolean}
      */
-    private selected = false;
+    selected = false;
+
+    /**
+     * Element actions.
+     */
+    el:ElementRef;
+    initIntervalCount:number = 0;
+    initInterval:any;
+    lastAction:number;
+    lastActionInterval:any;
 
     /**
      * @constructor
      * @param {ElementRef} el the element to be bound on
      */
-    constructor(el:ElementRef) {
+    constructor(el: ElementRef) {
+        this.el = el;
         //noinspection TypeScriptUnresolvedFunction
         Observable.fromEvent(el.nativeElement, 'keyup')
-            .filter((keyEvent:KeyboardEvent) => keyEvent.keyCode === 13 || keyEvent.keyCode === 27
-                                                || keyEvent.keyCode === 40 || keyEvent.keyCode === 38)
-            .subscribe((keyEvent:KeyboardEvent) => this.navigate(keyEvent));
+            .do(() => this.updateLastAction())
+            .filter((keyEvent: KeyboardEvent) => keyEvent.keyCode === 13 ||  keyEvent.keyCode === 27
+                || keyEvent.keyCode === 40 || keyEvent.keyCode === 38)
+            .subscribe((keyEvent: KeyboardEvent) => this.navigate(keyEvent));
 
         //noinspection TypeScriptUnresolvedFunction
         Observable.fromEvent(el.nativeElement, 'keyup')
-            .filter((keyEvent:KeyboardEvent) => keyEvent.keyCode !== 13 && keyEvent.keyCode !== 27
-                                                && keyEvent.keyCode !== 40 && keyEvent.keyCode !== 38)
+            .do(() => this.updateLastAction())
+            .filter((keyEvent: KeyboardEvent) => keyEvent.keyCode !== 13 && keyEvent.keyCode !== 27
+                && keyEvent.keyCode !== 40 && keyEvent.keyCode !== 38)
             .map(() => el.nativeElement.value)
             .do(() => {
                 this.suggestions = [];
@@ -68,19 +83,19 @@ export class SuggestDirective implements OnInit {
             .debounceTime(400)
             .switchMap((term) => (term.length > 1) ? this.mapper(el.nativeElement.value) : [])
             .subscribe(
-                (item) => {
-                    this.suggestions.push(item);
-                    this.offset = 0;
+            (item) => {
+                this.suggestions.push(item);
+                this.offset = 0;
 
-                    this.events.emit({
-                        type: SuggestEvents.CHANGED,
-                        data: this.suggestions
-                    });
-                },
-                (error) => this.events.emit({
-                    type: SuggestEvents.ERROR,
-                    data: error
-                }));
+                this.events.emit({
+                    type: SuggestEvents.CHANGED,
+                    data: this.suggestions
+                });
+            },
+            (error) => this.events.emit({
+                type: SuggestEvents.ERROR,
+                data: error
+            }));
     }
 
     /**
@@ -88,6 +103,7 @@ export class SuggestDirective implements OnInit {
      */
     ngOnInit() {
         this.events.subscribe((event) => {
+            this.updateLastAction();
             switch (event.type) {
                 case SuggestEvents.SELECTED:
                     this.suggestions = [];
@@ -105,15 +121,69 @@ export class SuggestDirective implements OnInit {
                         type: SuggestEvents.CHANGED,
                         data: this.suggestions
                     });
+                    break;
             }
         });
+
+        this.initInterval = setInterval(this.mouseListener.bind(this), 1000);
+        this.lastActionInterval = setInterval(this.checkLastAction.bind(this), 1000);
+    }
+
+    /**
+     * Check if mouse entered the component or leaves it.
+     */
+    mouseListener() {
+        if (!this.view) {
+            console && console.warn && console.warn('View not found. Retry ' + this.initIntervalCount, this.el);
+            if (this.initIntervalCount === 5) {
+                clearInterval(this.initInterval);
+            }
+            this.initIntervalCount++;
+            return;
+        }
+        clearInterval(this.initInterval);
+        
+        this.view.addEventListener('mouseover', () => {
+            this.updateLastAction();
+        })
+
+        this.view.addEventListener('mouseleave', () => {
+            this.suggestions = [];
+            this.events.emit({
+                type: SuggestEvents.CHANGED,
+                data: this.suggestions
+            });
+        });
+    }
+
+    /**
+     * Updates the last action time.
+     */
+    updateLastAction() {
+        this.lastAction = new Date().getTime();
+    }
+
+    /**
+     * Checks if user is inactive for the last x seconds.
+     */
+    checkLastAction() {
+        const time = new Date().getTime();
+        const difference = time - this.lastAction;
+
+        if (difference > 5000) {
+            this.suggestions = [];
+            this.events.emit({
+                type: SuggestEvents.CHANGED,
+                data: this.suggestions
+            });
+        }
     }
 
     /**
      * Executed on operator keys (arrow keys, enter, esc).
      * @param {KeyboardEvent} keyEvent the keyboard event
      */
-    private navigate(keyEvent:KeyboardEvent) {
+    private navigate(keyEvent: KeyboardEvent) {
         keyEvent.preventDefault();
         const keyCode = keyEvent.keyCode;
 
@@ -127,7 +197,7 @@ export class SuggestDirective implements OnInit {
             return;
         }
 
-        if (!this.selected || keyCode === 40 && (this.offset + 1) >= this.suggestions.length) {
+        if (!this.selected ||  keyCode === 40 && (this.offset + 1) >= this.suggestions.length) {
             this.offset = 0;
             this.selected = true;
         } else if (keyCode === 40) {
