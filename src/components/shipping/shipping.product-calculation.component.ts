77b6334e3659882e20d99bea1d5f131d0e644e43
Dominik Weidenfeld <dominik.weidenfeld@twt.de>
2016-07-15 10:42:39 +0200

    validate input numbers

--- src/components/shipping/shipping.product-calculation.component.ts
+++ src/components/shipping/shipping.product-calculation.component.ts
@@ -67,9 +67,13 @@ export default class ShippingProductCalculationComponent {
     /**
      * Parcel Information.
      */
+    validWeightInput: boolean = true;
     weightInputChange: EventEmitter<number> = new EventEmitter();
+    validLengthInput: boolean = true;
     lengthInputChange: EventEmitter<number> = new EventEmitter();
+    validWidthInput: boolean = true;
     widthInputChange: EventEmitter<number> = new EventEmitter();
+    validHeightInput: boolean = true;
     heightInputChange: EventEmitter<number> = new EventEmitter();
     @Output() dimensionsChange: EventEmitter<DimensionInfo> = new EventEmitter();
     @Output() weightChange: EventEmitter<WeightInfo> = new EventEmitter();
@@ -93,35 +97,66 @@ export default class ShippingProductCalculationComponent {
      * Bind all events.
      */
     private bindEvents() {
-        this.weightInputChange.subscribe((value: number) => {
-            this.parcelInfo.Characteristic.Weight.Value = value;
-            this.weightChange.emit(this.parcelInfo.Characteristic.Weight);
+        const resetParcel = () => {
             this.parcelSuggestions = [];
             this.parcelChange.emit(null);
+        };
+
+        this.weightInputChange.subscribe((value: string) => {
+            resetParcel();
+
+            const num = this.parseNumber(value);
+            if (num || value === '') {
+                this.validWeightInput = true;
+                this.parcelInfo.Characteristic.Weight.Value = num || null;
+                this.weightChange.emit(this.parcelInfo.Characteristic.Weight);
+            } else {
+                this.validWeightInput = false;
+            }
         });
 
-        this.lengthInputChange.subscribe((value: number) => {
-            this.parcelInfo.Characteristic.Dimension.Length = value;
-            this.dimensionsChange.emit(this.parcelInfo.Characteristic.Dimension);
-            this.parcelSuggestions = [];
-            this.parcelChange.emit(null);
+        this.lengthInputChange.subscribe((value: string) => {
+            resetParcel();
+
+            const num = this.parseNumber(value);
+            if (num || value === '') {
+                this.validLengthInput = true;
+                this.parcelInfo.Characteristic.Dimension.Length = num;
+                this.dimensionsChange.emit(this.parcelInfo.Characteristic.Dimension);
+            } else {
+                this.validLengthInput = false;
+            }
         });
 
-        this.widthInputChange.subscribe((value: number) => {
-            this.parcelInfo.Characteristic.Dimension.Width = value;
-            this.dimensionsChange.emit(this.parcelInfo.Characteristic.Dimension);
-            this.parcelSuggestions = [];
-            this.parcelChange.emit(null);
+        this.widthInputChange.subscribe((value: string) => {
+            resetParcel();
+            
+            const num = this.parseNumber(value);
+            if (num || value === '') {
+                this.validWidthInput = true;
+                this.parcelInfo.Characteristic.Dimension.Width = num;
+                this.dimensionsChange.emit(this.parcelInfo.Characteristic.Dimension);
+            } else {
+                this.validWidthInput = false;
+            }
         });
 
-        this.heightInputChange.subscribe((value: number) => {
-            this.parcelInfo.Characteristic.Dimension.Height = value;
-            this.dimensionsChange.emit(this.parcelInfo.Characteristic.Dimension);
-            this.parcelSuggestions = [];
-            this.parcelChange.emit(null);
+        this.heightInputChange.subscribe((value: string) => {
+            resetParcel();
+
+            const num = this.parseNumber(value);
+            if (num || value === '') {
+                this.validHeightInput = true;
+                this.parcelInfo.Characteristic.Dimension.Height = num;
+                this.dimensionsChange.emit(this.parcelInfo.Characteristic.Dimension);
+            } else {
+                this.validHeightInput = false;
+            }
         });
 
         this.isDocumentChange.subscribe((value: boolean) => {
+            resetParcel();
+
             this.parcelInfo.Characteristic.IsDocument = value;
             this.lengthInputChange.emit(null);
             this.widthInputChange.emit(null);
@@ -197,5 +232,18 @@ export default class ShippingProductCalculationComponent {
     public parcelSelected(parcel: PostalProductInfo) {
         this.parcelChange.emit(parcel);
         this.modalProductSelect.close();
+    }
+
+    /**
+     * Parse a number if valid.
+     * @param {string} input the possible number
+     * @return {number}
+     */
+    private parseNumber(input: string): number {
+        const n = parseFloat(input);
+        if (input && input.match(/^[0-9]+(\.?[0-9]+)?$/) && !Number.isNaN(n) && Number.isFinite(n)) {
+            return n;
+        }
+        return null;
     }
 }
