dc8c1958ea2a422ec881a8758c36627405a9e2e8
Andreas Wacknitz <a.wacknitz@francotyp.com>
2016-08-01 14:01:42 +0200

    Add missing errorHandler().

--- src/components/administration/administration.component.ts
+++ src/components/administration/administration.component.ts
@@ -1,6 +1,8 @@
 import {Component} from '@angular/core';
 import {TranslatePipe} from 'ng2-translate/ng2-translate';
 import AdministrationAddressgroupComponent from './administration.addressgroup.component';
+import ErrorUtils from "../../utils/error-utils";
+
 
 @Component({
     selector: 'fp-administration',
@@ -21,5 +23,16 @@ export default class AdministrationComponent {
      * Error if there is one.
      */
     error: Error;
+
+
+    /**
+     * Handle errors.
+     * @param {any} error the error
+     */
+    public handleError(error: any) {
+        console && console.warn && console.warn(error);
+        this.error = ErrorUtils.toError(error);
+        document.getElementsByTagName('body')[0].scrollIntoView();
+    }
 
 }
