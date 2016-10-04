970095dca4961f19a77264328cb27912ab5bffb2
Andreas Wacknitz <a.wacknitz@francotyp.com>
2016-10-04 10:09:20 +0200

    Fix subscription problem (user properties have only been showed after refresh).

--- src/components/user-properties.component.ts
+++ src/components/user-properties.component.ts
@@ -1,6 +1,7 @@
-﻿import {Component, EventEmitter, Output} from '@angular/core';
+﻿import {NgZone, Component, EventEmitter, Output, AfterViewInit} from '@angular/core';
 import {TranslatePipe} from 'ng2-translate/ng2-translate';
 import CustomerService from "../services/customer.service";
+import LoginService from "../services/login.service";
 import ConsumerInfo from "../models/consumer-info";
 
 
@@ -15,23 +16,34 @@ import ConsumerInfo from "../models/consumer-info";
 /**
  * UserProperties component.
  */
-export default class UserPropertiesComponent {
+export default class UserPropertiesComponent implements AfterViewInit {
     @Output() onError: EventEmitter<Error> = new EventEmitter<Error>();
 
     /**
      * Consumer information.
      */
-    consumer: ConsumerInfo;
+    //consumer: ConsumerInfo;
 
-    loginName:  string = "";
-    customerNo: string = "";
+    loginName:  string;
+    customerNo: string;
     /**
      * @constructor
      * @param {CustomerService} customerService the customer client
      */
-    constructor(private customerService: CustomerService) {
+    constructor(private customerService: CustomerService, private loginService: LoginService, private zone: NgZone) {
+    }
+
+    ngAfterViewInit(): void {
+        this.loginService.loginChange.subscribe((event) => {
+            if (event.loggedIn) {
+                this.getCustomerInfo();
+            }
+        });
+    }
+
+    private getCustomerInfo(): void {
         this.customerService.getConsumerInfo().subscribe(
-            (r: ConsumerInfo) => { this.loginName = r.Login; this.customerNo = r.Customer.CustomerNumber; },
+            (r: ConsumerInfo) => this.zone.run(() => { this.loginName = r.Login; this.customerNo = r.Customer.CustomerNumber; }),
             (error: Error) => this.onError.emit(error));
     }
 }
